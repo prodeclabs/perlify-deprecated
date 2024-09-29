dee969e47ce113a841af33d2e4ef8652cec95174
Shadev Kumar <Shadevsrk261@gmail.com>
2024-09-29 20:58:36 +0530

    chore: fixing type error in prsima.ts file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,7 +5,16 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma =
-  globalThis.prisma || new PrismaClient().$extends(withAccelerate());
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate());
+};
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
