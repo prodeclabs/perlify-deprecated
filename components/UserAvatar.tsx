a690b6a678ba5871294887e0bb9bf09da0b9d6c8
Sourabh <rathoursourabh5@gmail.com>
2024-09-29 11:55:25 +0530

    chore: setting up biome.js as linter and formatter

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 import { auth } from "../auth";
 
 export default async function UserAvatar() {
-  const session = await auth();
+	const session = await auth();
 
-  if (!session?.user) return null;
+	if (!session?.user) return null;
 
-  return (
-    <div className="flex items-center gap-4">
-      <Image
-        width={20}
-        height={20}
-        src={session.user.image!}
-        alt="User Avatar"
-      />
-      <p>{session.user.name}</p>
-    </div>
-  );
+	return (
+		<div className="flex items-center gap-4">
+			<Image
+				width={20}
+				height={20}
+				src={session.user.image!}
+				alt="User Avatar"
+			/>
+			<p>{session.user.name}</p>
+		</div>
+	);
 }
